40cbbc55212377e2d62245a1a5d803dc5546971e
faizannadeem12 <37186212+faizannadeem12@users.noreply.github.com>
2018-06-24 22:25:38 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+clear
+echo "
+▒███████▒▓█████  ███▄    █  ▄████▄   ▒█████     ▄▄▄█████▓ ▒█████   ▒█████   ██▓    
+▒ ▒ ▒ ▄▀░▓█   ▀  ██ ▀█   █ ▒██▀ ▀█  ▒██▒  ██▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    
+░ ▒ ▄▀▒░ ▒███   ▓██  ▀█ ██▒▒▓█    ▄ ▒██░  ██▒   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    
+  ▄▀▒   ░▒▓█  ▄ ▓██▒  ▐▌██▒▒▓▓▄ ▄██▒▒██   ██░   ░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░    
+▒███████▒░▒████▒▒██░   ▓██░▒ ▓███▀ ░░ ████▓▒░     ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒
+░▒▒ ▓░▒░▒░░ ▒░ ░░ ▒░   ▒ ▒ ░ ░▒ ▒  ░░ ▒░▒░▒░      ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░
+░░▒ ▒ ░ ▒ ░ ░  ░░ ░░   ░ ▒░  ░  ▒     ░ ▒ ▒░        ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░
+░ ░ ░ ░ ░   ░      ░   ░ ░ ░        ░ ░ ░ ▒       ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   
+  ░ ░       ░  ░         ░ ░ ░          ░ ░                  ░ ░      ░ ░      ░  ░
+░                          ░                                                       
+
+                                                ";
+
+echo "[✔] Checking directories...";
+if [ -d "/usr/share/doc/zenco" ] ;
+then
+echo "[◉] A directory zenco was found! Do you want to replace it? [Y/n]:" ; 
+read mama
+if [ $mama == "y" ] ; 
+then
+ rm -R "/usr/share/doc/zenco"
+else
+ exit
+fi
+fi
+
+ echo "[✔] Installing ...";
+ echo "";
+ git clone https://github.com/Manisso/zenco.git /usr/share/doc/zenco;
+ echo "#!/bin/bash 
+ python /usr/share/doc/zenco/zenco.py" '${1+"$@"}' > zenco;
+ chmod +x zenco;
+ sudo cp zenco /usr/bin/;
+ rm zenco;
+
+
+if [ -d "/usr/share/doc/zenco" ] ;
+then
+echo "";
+echo "[✔]Tool istalled with success![✔]";
+echo "";
+  echo "[✔]====================================================================[✔]";
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing zenco  !     ✔✔✔ [✔]"; 
+  echo "[✔]====================================================================[✔]";
+  echo "";
+else
+  echo "[✘] Installation faid![✘] ";
+  exit
+fi
